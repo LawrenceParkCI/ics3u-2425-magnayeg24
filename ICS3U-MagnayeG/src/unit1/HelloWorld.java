4a2748f97825fd4440c0a9ef517f705e6cc43c4b
gabrielmagnaye <gabrielmagnaye@localhost>
2024-09-20 11:11:15 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-MagnayeG/src/unit1/HelloWorld.java
@@ -0,0 +1,21 @@
+package unit1;
+/**
+ * Description: This program prints Hello World, and a second statement <p>
+ * Date: 09/19/2024
+ * @author Gabriel Magnaye
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("Hello World!!");
+		System.out.println("The Quick Brown Fox Jumped Over The Lazy Dog");
+		
+		/*
+		 * This is a block comment
+		 * You can add notes on multiple lines
+		 */
+	}
+
+	
+}

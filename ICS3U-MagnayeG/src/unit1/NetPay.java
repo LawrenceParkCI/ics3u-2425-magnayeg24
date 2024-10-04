ace4306ede701ab0c6d43ccfc4a71040d9ad3faa
gabrielmagnaye <gabrielmagnaye@localhost>
2024-10-04 11:21:09 -0400

    Completed netpay assignment

---
+++ ICS3U-MagnayeG/src/unit1/NetPay.java
@@ -0,0 +1,26 @@
+package unit1;
+/**
+ * Date: 10/04/2024
+ * Description: Calculates the netpay
+ * @author Gabriel Magnaye
+ */
+public class NetPay {
+	/**
+	 *This is the entry point to the program
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+	
+		//declare the variables
+		double netPay;
+		int hours = 40;
+		double wage = 5.00;
+		double insurance = 2.00;
+		double tax = 0.22;
+		netPay = (hours*wage-insurance) - tax*(hours*wage-insurance);
+
+		//display the netpay
+		System.out.println("The next pay is " + netPay);
+	}
+
+}

35cd80e082a36b438196f417ea42a81da47bbaec
gabrielmagnaye <gabrielmagnaye@localhost>
2024-10-17 09:31:59 -0400

    Completed decimal formatting worksheet

---
+++ ICS3U-MagnayeG/src/unit1/DFOutput.java
@@ -0,0 +1,77 @@
+package unit1;
+
+/**
+* Description: This is a decimal formatting worksheet
+* Date: 10/17/2024
+* @author Gabriel Magnaye
+*/
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class DFOutput {
+	
+	 /**
+	   * This is the entry point to the program.
+	   * @param args unused
+	   */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Run the following program with a cost of 10.00.
+		//What value is displayed in the output?
+		//11.3 is the output value
+		
+		final double TAX_RATE = 0.13;
+		DecimalFormat money = new DecimalFormat("$#,###,##0.00");
+		DecimalFormat percent = new DecimalFormat("0.##%");
+
+		double cost;
+		System.out.print("Please enter the cost of an item then press <Enter>: $");
+		cost = sc.nextDouble();
+		double total = cost + (cost * TAX_RATE);
+		
+		System.out.println("The cost of your item with tax is " + money.format(total));
+		
+
+		//Change the above print statement on line 23 to:
+		//System.out.println("The cost of your item with tax is " + money.format(total));
+		//Run the program again, how is the output different? Why?
+		//11.30 is output rather than 11.3 as variable is formatted to do so
+		
+		
+		//Modify the parameter for the DecimalFormat on line 15 to be "$0.00"
+		//Run the program again with the cost of 10, how is the output different?
+		//The program now outputs a dollar sign
+		
+		//Modify the parameter for the DecimalFormat on line 15 to be "$0.##"
+		//Run the program again with the cost of 10, how is the output different?
+		//The final zero in the number is removed
+		
+		//Modify the parameter for the DecimalFormat on line 15 to be "$0.##"
+		//Calculate 10.50 x 1.13 = 11.865
+		//Run the program again with the cost of 10.50, what is happening?
+		//The value is rounded up to two decimal points
+		
+		//Modify the parameter for the DecimalFormat on line 15 as you think is appropriate for money.
+		//Run the program again with the cost of 123456, what is displayed?
+		//The value $139505.28 is output
+		
+		//Modify the parameter for the DecimalFormat on line 15 to be "$#,###,##0.00"
+		//Run the program again with the cost of 123456, how is the output different? Explain what is happening.
+		//The value is now formatted with commas. This occurs because the numbers are being inputed to where the # are and replacing them
+		
+		
+		//Uncomment the following print line, and run the program with any value for the cost.
+		//What value is printed for the TAX_RATE?
+		System.out.println("The tax rate is " + percent.format(TAX_RATE));
+		//The tax rate for any amount is 0.13
+		
+		//Change the above print statement to format the TAX_RATE using the percent DecimalFormat instance.
+		//Run the program again with any cost, how is the TAX_RATE displayed differently?
+
+
+		sc.close();
+	}
+
+}

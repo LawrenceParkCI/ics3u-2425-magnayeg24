cccdf139c07203201111f37cf429c060d1c1f096
gabrielmagnaye <gabrielmagnaye@localhost>
2024-11-13 09:00:39 -0500

    Completed string challenge

---
+++ ICS3U-MagnayeG/src/Unit2/StringChallenge.java
@@ -0,0 +1,25 @@
+package Unit2;
+
+import java.util.Scanner;
+
+/**
+ * Description: This is a string challenge worksheet
+ * Date: 11/11/2024
+ * @author Gabriel Magnaye
+*/
+
+public class StringChallenge {
+  public static void main(String[] args) {
+    //This is for the portfolio
+
+    /*
+    Create a program that demonstrates the use of .equals(), .equalsIgnoreCase(), .compareTo().  You should be using if statements for this program.
+
+    Also, include an example of explicit and implicit casting.
+
+    Comment to explain:
+      -when you use the different String functions
+      -when you are casting and how the value is changed
+    */
+  }
+}

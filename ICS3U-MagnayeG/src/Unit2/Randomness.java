0c9d7cbf01df1d2332a27e4c045cc8e9ba51b85e
gabrielmagnaye <gabrielmagnaye@10.123.163.66>
2024-11-18 12:10:24 -0500

    Completed randomness worksheet

---
+++ ICS3U-MagnayeG/src/Unit2/Randomness.java
@@ -0,0 +1,27 @@
+package Unit2;
+
+/**
+
+https://www.geeksforgeeks.org/java-math-random-method-examples/
+
+Description: This is a randomness worksheet
+Date: 11/18/2024
+@author: Gabriel Magnaye
+*/
+
+
+public class Randomness {
+  public static void main(String[] args) {
+    for (int i = 0; i < 1; i++) {
+      System.out.println((Math.random()*900) + 100);
+    }
+
+    //Run the program a few times. What do you think is the 
+    //range of the numbers generated?
+    //Greater than zero, less than one
+
+    //Read the demo program. Write the code so that the program 
+    //will produce a random number between 100 and 1000.
+    
+  }
+}

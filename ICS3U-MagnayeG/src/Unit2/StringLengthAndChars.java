b997424c5bb3b544a6c8c8f81d63315b329bc18e
gabrielmagnaye <gabrielmagnaye@gabriels-air>
2024-12-23 09:30:41 -0500

    Completed string length and chars worksheet

---
+++ ICS3U-MagnayeG/src/Unit2/StringLengthAndChars.java
@@ -0,0 +1,37 @@
+package Unit2;
+/**
+Description: This is a string length and chars worksheet
+Date: 12/23/2024
+@author Gabriel Magnaye 
+ */
+
+public class StringLengthAndChars {
+
+	/**
+	 * @param args unused
+	 */
+
+	public static void main(String[] args) {
+		/*
+      Goal: Using string methods with a for loop
+        .length()
+        .charAt(<position>)
+		 */
+
+		String name = "Ms. Kemp";
+
+		//write comments for the following code - what does it print out, and why?
+		System.out.println(name.length());
+		System.out.println(name.charAt(1));
+		System.out.println(name.charAt(2));
+		System.out.println(name.charAt(3));
+
+		//.length() gets the _____________
+		//.charAt(position) gets the _______________
+		//counting starts at _____________
+
+		/**
+      Ask for user input for name. Print out each individual letter of the name on separate lines, using a for loop
+		 */
+	}
+}
